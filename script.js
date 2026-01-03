9d0dd78d6c0e1f293ce15b385529759464d5fc49
bradens.bhs.tsa <bradens.bhs.tsa@gmail.com>
2026-01-03 22:15:35 +0000

    v1 yay

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let map;
 let geocoder;
 let infoWindow;
 let markers = [];
+let filteredPlaces;
+let checkedPlaces;
+let routePolyline;
 
 async function initMap() {
   const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
   const { Place } = await google.maps.importLibrary("places");
   const { Geocoder } = await google.maps.importLibrary("geocoding");
 
@@ -20,6 +22,13 @@ async function initMap() {
   infoWindow = new InfoWindow();
 
   document.getElementById("search-button").addEventListener("click", performSearch);
+  document.getElementById("location-input").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      performSearch();
+    }
+  });
+  document.getElementById("clear-results-btn").addEventListener("click", clearResults);
+  document.getElementById('calculate-route-button').addEventListener("click", calculateBestRoute);
 }
 
 async function performSearch() {
@@ -44,23 +53,64 @@ async function performSearch() {
   messageBox.style.display = "block";
 
   try {
-    const geocodeResult = await geocoder.geocode({ address: location });
-    if (geocodeResult.results && geocodeResult.results.length > 0) {
-      const locationGeometry = geocodeResult.results[0].geometry;
-      map.fitBounds(locationGeometry.viewport);
+    const geocodeResults = await geocoder.geocode({ address: location, language: 'en' });
+    if (geocodeResults.results && geocodeResults.results.length > 0) {
+      const geocodeResult = geocodeResults.results[0]
+      const localityName = geocodeResult.address_components
+        .find(component => component.types.includes("locality")
+        ).long_name;
+      const countyName = geocodeResult.address_components
+        .find(component => component.types.includes("administrative_area_level_2")
+        ).long_name;
+      const matchFields = [];
+      if (document.getElementById("city-match").checked) {
+        matchFields.push(["locality", localityName]);
+      }
+      if (document.getElementById("county-match").checked) {
+        matchFields.push(["administrative_area_level_2", countyName]);
+      }
+      const viewport = geocodeResult.geometry.viewport;
+      map.fitBounds(viewport);
 
       const { Place } = await google.maps.importLibrary("places");
+
       const request = {
         textQuery: query,
-        fields: ["displayName", "location", "formattedAddress"],
-        locationRestriction: locationGeometry.viewport,
+        fields: ["displayName", "location", "formattedAddress", "addressComponents"],
+        locationRestriction: viewport
       };
+      let { places } = await Place.searchByText(request)
 
-      const { places } = await Place.searchByText(request);
+      if (places.length == 20) {
+        // Search by quadrant if there are more than 20 places (max)
+        const quads = await getViewportQuadrants(viewport)
+        const promises = quads.map((quad) =>
+          Place.searchByText({
+            ...request,
+            locationRestriction: quad
+          })
+        );
+        const results = await Promise.all(promises);
+        places = results.flatMap(result => result.places);
+      }
 
-      if (places.length) {
+      filteredPlaces = places.filter(
+        (place) => matchFields.every(
+          ([matchType, matchValue]) => place.addressComponents.some(
+            (component) =>
+              component.types.includes(matchType) && component.longText.includes(matchValue)
+          )
+        )
+      );
+      checkedPlaces = new Set(filteredPlaces.map(place => place.id));
+
+      // console.log(places.map(p => p.addressComponents.map(c => [c.longText, c.types])), matchFields, filteredPlaces)
+
+
+      if (filteredPlaces.length) {
         messageBox.style.display = "none";
-        displayResults(places);
+        displayResults(query, location, filteredPlaces);
+        document.getElementById('route-box').classList.remove("hidden");
       } else {
         messageBox.textContent = "No results found in the specified area.";
       }
@@ -73,45 +123,109 @@ async function performSearch() {
   }
 }
 
+async function getViewportQuadrants(viewport) {
+  const { LatLngBounds } = await google.maps.importLibrary("core")
+  // split one bounds into quadrants
+  const north = viewport.getNorthEast().lat();
+  const east = viewport.getNorthEast().lng();
+  const south = viewport.getSouthWest().lat();
+  const west = viewport.getSouthWest().lng();
+  const midLat = viewport.getCenter().lat();
+  const midLng = viewport.getCenter().lng();
+
+  return [
+    new LatLngBounds({ north, east, south: midLat, west: midLng }),
+    new LatLngBounds({ north, east: midLng, south: midLat, west }),
+    new LatLngBounds({ north: midLat, east: midLng, south, west }),
+    new LatLngBounds({ north: midLat, east, south, west: midLng }),
+  ]
+
+}
+
 function clearResults() {
   markers.forEach((marker) => {
     marker.map = null;
   });
   markers = [];
+  document.getElementById("search-form").classList.remove("hidden");
+  document.getElementById("results-panel").classList.add("hidden");
+  document.getElementById("route-box").classList.add("hidden");
   document.getElementById("results-list").innerHTML = "";
   document.getElementById("message-box").style.display = "none";
   infoWindow.close();
-}
-
-function displayResults(places) {
-  const resultsList = document.getElementById("results-list");
-  const bounds = new google.maps.LatLngBounds();
-
-  places.forEach((place) => {
-    const marker = createMarker(place);
-    bounds.extend(place.location);
-
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `
-      <div class="place-name">${place.displayName}</div>
-      <div class="place-address">${place.formattedAddress}</div>
-    `;
-    listItem.addEventListener("click", () => {
-      google.maps.event.trigger(marker, "click");
-    });
-    resultsList.appendChild(listItem);
-  });
-
-  if (places.length > 1) {
-      map.fitBounds(bounds);
-  } else if (places.length === 1) {
-      map.setCenter(places[0].location);
-      map.setZoom(15);
+  if (routePolyline) {
+    routePolyline.setMap(null);
   }
 }
 
-function createMarker(place) {
-  const marker = new google.maps.marker.AdvancedMarkerElement({
+function displayResults(query, location, places) {
+  document.getElementById("results-panel").classList.remove("hidden");
+  document.getElementById('search-form').classList.add("hidden");
+  const resultsList = document.getElementById("results-list");
+  const resultSummary = document.getElementById("results-summary");
+  const bounds = new google.maps.LatLngBounds();
+
+  resultSummary.innerHTML = `
+  <h2>
+    Found ${places.length} ${query} locations
+  </h2>
+  <h3>
+    In ${location}
+  </h3>
+  `
+
+  places.forEach((place) => {
+    bounds.extend(place.location);
+  });
+  places.forEach(async (place) => {
+    const marker = await createMarker(place);
+    resultsList.appendChild(createNewResult(place, marker));
+  });
+
+
+  if (places.length > 1) {
+    map.fitBounds(bounds);
+  } else if (places.length === 1) {
+    map.setCenter(places[0].location);
+    map.setZoom(15);
+  }
+}
+
+function createNewResult(place, marker) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("flex");
+  listItem.innerHTML = `
+      <div>
+        <div class="place-name">${place.displayName}</div>
+        <div class="place-address">${place.formattedAddress}</div>
+      </div>
+    `;
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.id = `place-checkbox-${place.id})`;
+  checkbox.checked = true;
+  checkbox.addEventListener("click", (event) => {
+    event.stopPropagation();
+    if (checkbox.checked) {
+      checkedPlaces.add(place.id);
+      marker.setMap(map);
+    } else {
+      marker.setMap(null);
+      checkedPlaces.delete(place.id);
+    }
+  });
+  listItem.prepend(checkbox)
+  listItem.addEventListener("click", () => {
+    if (!checkbox.checked) return;
+    google.maps.event.trigger(marker, "click");
+  });
+  return listItem;
+}
+
+async function createMarker(place) {
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  const marker = new AdvancedMarkerElement({
     map,
     position: place.location,
     title: place.displayName,
@@ -128,6 +242,66 @@ function createMarker(place) {
 
   markers.push(marker);
   return marker;
+}
+
+async function calculateBestRoute() {
+  const selectedPlaces = filteredPlaces.filter((place) => checkedPlaces.has(place.id));
+  // console.log('calculating route for', checkedPlaces, selectedPlaces, selectedPlaces.map(p => p.location))
+  if (selectedPlaces.length < 2) {
+    const message = 'Need at least 2 locations to calculate a route.';
+    console.warn(message);
+    const routeInfoDiv = document.getElementById('route-info');
+    routeInfoDiv.innerHTML = message;
+    routeInfoDiv.classList.remove('hidden');
+    return;
+  }
+
+  if (routePolyline) {
+    routePolyline.setMap(null);
+  }
+
+  const { Route } = await google.maps.importLibrary("routes");
+
+  const waypoints = selectedPlaces.map(place => ({ location: place.location }));
+  const origin = waypoints.shift();
+
+  const request = {
+    origin: origin,
+    destination: origin,
+    intermediates: waypoints,
+    travelMode: 'BICYCLING',
+    optimizeWaypointOrder: true,
+    fields: ['path', 'legs', 'viewport', 'localizedValues', 'warnings', 'distanceMeters', 'durationMillis', 'optimizedIntermediateWaypointIndices']
+  };
+
+  const { routes } = await Route.computeRoutes(request);
+
+  if (routes && routes.length > 0) {
+    const route = routes[0];
+    // console.log("best route", route, route.localizedValues);
+    [routePolyline] = route.createPolylines({
+      polylineOptions: {
+        map: map,
+        strokeColor: '#1a73e8',
+        strokeWeight: 6,
+        strokeOpacity: 0.8,
+      },
+    });
+
+    const routeInfoDiv = document.getElementById('route-info');
+    const distance = route.localizedValues?.distance || 'N/A';
+    const duration = route.localizedValues?.duration || 'N/A';
+    routeInfoDiv.innerHTML = `<strong>Best Route:</strong> ${distance} / ${duration}`;
+    routeInfoDiv.classList.remove('hidden');
+
+    map.fitBounds(route.viewport);
+  } else {
+    const message = 'Could not calculate a route.';
+    console.error(message);
+    const routeInfoDiv = document.getElementById('route-info');
+    routeInfoDiv.innerHTML = message;
+    routeInfoDiv.classList.remove('hidden');
+  }
 }
 
 initMap();
